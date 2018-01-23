389c2cec512cabba4c3ab4a5b9172569ad2db83d
Robert <robertkim54@gmail.com>
2018-01-23 15:41:34 -0500

    Data retrieved on time interval successfully

--- app/src/main/java/com/srp/rkim/argus/GPSService.java
+++ app/src/main/java/com/srp/rkim/argus/GPSService.java
@@ -3,6 +3,7 @@ package com.srp.rkim.argus;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -17,9 +18,12 @@ import java.util.Calendar;
 public class GPSService extends Service {
     private static final String TAG = "GPSService";
     private static final int MAP_PERMISSIONS = 1;
+    private static final long INTERVAL = 1000 * 60 * 1;
     Context context;
     GoogleMap map;
     GPSTracker gps;
+    FirebaseDatabase mDatabase;
+    DatabaseReference myRef;
     private FirebaseAuth mAuth;
     private FirebaseUser user;
 
@@ -31,7 +35,48 @@ public class GPSService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.e(TAG, "onStartCommand");
+        context = getApplicationContext();
+
+
         super.onStartCommand(intent, flags, startId);
+        gps = new GPSTracker(context);
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = mDatabase.getReference();
+
+
+        final Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Do something after 5s = 5000ms
+                // Check if GPS enabled
+                if (gps.canGetLocation()) {
+                    Log.e(TAG, "gps enabled");
+//            Log.e(TAG, myRef.toString());
+                    Log.e(TAG, Calendar.getInstance().getTime().toString());
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+                    myRef.child("users").child(user.getUid()).child("location").child("latitude").setValue(latitude);
+                    myRef.child("users").child(user.getUid()).child("location").child("longitude").setValue(longitude);
+                    myRef.child("users").child(user.getUid()).child("time").setValue(Calendar.getInstance().getTime());
+                    // \n is for new line
+                } else {
+                    Log.e(TAG, "gps disabled");
+                    gps.showSettingsAlert();
+                }
+                handler.postDelayed(this, INTERVAL);
+            }
+        }, INTERVAL);
+
+
+        // Can't get location.
+        // GPS or network is not enabled.
+        // Ask user to enable GPS/network in settings.
+
+
         return START_STICKY;
     }
 
@@ -39,18 +84,22 @@ public class GPSService extends Service {
     public void onCreate() {
         Log.e(TAG, "onCreate");
         context = getApplicationContext();
-        mAuth = FirebaseAuth.getInstance();
-        user = mAuth.getCurrentUser();
-        FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
-        final DatabaseReference myRef = mDatabase.getReference();
+
         //  String[] perms = {"android.permission.ACCESS_COARSE_LOCATION", "android.permission.ACCESS_FINE_LOCATION"};
         //   requestPermissions(context, perms, MAP_PERMISSIONS);
 
         gps = new GPSTracker(context);
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = mDatabase.getReference();
+
 
         // Check if GPS enabled
         if (gps.canGetLocation()) {
-            Log.d(TAG, "gps enabled");
+            Log.e(TAG, "gps enabled");
+//            Log.e(TAG, myRef.toString());
+            Log.e(TAG, Calendar.getInstance().getTime().toString());
             double latitude = gps.getLatitude();
             double longitude = gps.getLongitude();
             myRef.child("users").child(user.getUid()).child("location").child("latitude").setValue(latitude);
@@ -58,7 +107,7 @@ public class GPSService extends Service {
             myRef.child("users").child(user.getUid()).child("time").setValue(Calendar.getInstance().getTime());
             // \n is for new line
         } else {
-            Log.d(TAG, "gps disabled");
+            Log.e(TAG, "gps disabled");
 
             // Can't get location.
             // GPS or network is not enabled.
